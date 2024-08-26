df68e65499b01075e7addfbba82e484430a546a2
Alexander John <96aljohn@gmail.com>
2024-08-25 20:03:25 -0700

    correct-index

--- index.mjs
+++ index.mjs
@@ -4,49 +4,15 @@ import { Neo4jGraphQL } from "@neo4j/graphql";
 import neo4j from "neo4j-driver";
 
 const typeDefs = `#graphql
-    type Business {
-    businessId: ID!
-    waitTime: Int! @computed
-    averageStars: Float!
-      @cypher(
-        statement: "MATCH (this)<-[:REVIEWS]-(r:Review) RETURN avg(r.stars)"
-      )
-    recommended(first: Int = 1): [Business!]!
-      @cypher(
-        statement: """
-        MATCH (this)<-[:REVIEWS]-(:Review)<-[:WROTE]-(:User)-[:WROTE]->(:Review)-[:REVIEWS]->(rec:Business)
-        WITH rec, COUNT(*) AS score
-        RETURN rec ORDER BY score DESC LIMIT $first
-        """
-      )
-    name: String!
-    city: String!
-    state: String!
-    address: String!
-    location: Point!
-    reviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
-    categories: [Category!]! @relationship(type: "IN_CATEGORY", direction: OUT)
+    type Movie {
+        title: String
+        actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
     }
 
-    type User {
-    userID: ID!
-    name: String!
-    reviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
-  }
-
-  type Review {
-    reviewId: ID!
-    stars: Float!
-    date: Date!
-    text: String
-    user: User! @relationship(type: "WROTE", direction: IN)
-    business: Business! @relationship(type: "REVIEWS", direction: OUT)
-  }
-
-  type Category {
-    name: String!
-    businesses: [Business!]! @relationship(type: "IN_CATEGORY", direction: IN)
-  }
+    type Actor {
+        name: String
+        movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+    }
 `;
 
 const driver = neo4j.driver(
