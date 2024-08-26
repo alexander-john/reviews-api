df9c5bcb626af433e933b596b8b8e09916e7ea68
Alexander John <96aljohn@gmail.com>
2024-08-25 19:55:47 -0700

    create-index

---
+++ index.mjs
@@ -0,0 +1,17 @@
+const driver = neo4j.driver(
+    "bolt://localhost:7687",
+    neo4j.auth.basic("neo4j", "letmein")
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+const server = new ApolloServer({
+    schema: await neoSchema.getSchema(),
+});
+
+const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => ({ req }),
+    listen: { port: 4000 },
+});
+
+console.log(`🚀 Server ready at ${url}`);
